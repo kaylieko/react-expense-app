c092815d26ba044a4ba871227cbf1b0f20c30241
Kaylie Ko <kko@rioseo.com>
2021-01-16 22:21:31 -0800

    createContext for transactions

---
+++ src/context/GlobalState.js
@@ -0,0 +1,22 @@
+import React, { createContext, useReducer } from 'react';
+
+const initialState = {
+    transations: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({children}) => {
+	return (
+		<GlobalContext.Provider>
+			{children}
+		</GlobalContext.Provider>
+	)
+}
