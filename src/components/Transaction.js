77799e800c959773c0822c652f0840432051e520
Kaylie Ko <kko@rioseo.com>
2021-01-18 12:43:13 -0800

    add 'delete transaction' to appReducer

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
 export const Transaction = ({transaction}) => {
+	const { deleteTransaction } = useContext(GlobalContext);
 	const sign = transaction.amount < 0 ? '-' : '+';
 
 	return (
 		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
     		{transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-    		<button className="delete-btn">x</button>
+    		<button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
 		</li>
 	)
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,7 +2,7 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-    transations: [
+    transactions: [
         { id: 1, text: 'Flower', amount: -20 },
         { id: 2, text: 'Salary', amount: 300 },
         { id: 3, text: 'Book', amount: -10 },
@@ -15,10 +15,20 @@ export const GlobalContext = createContext(initialState);
 
 // Provider component
 export const GlobalProvider = ({children}) => {
-	const [state, dispatch] = useReducer(AppReducer, initialState);
+  	const [state, dispatch] = useReducer(AppReducer, initialState);
 
+	//Actions
+	function deleteTransaction(id){
+		dispatch({
+			type: 'DELETE_TRANSACTION',
+			payload: id
+		});
+	}
 	return (
-		<GlobalContext.Provider value={{transactions: state.transations}}>
+		<GlobalContext.Provider value={{
+			transactions: state.transactions,
+			deleteTransaction
+		}}>
 			{children}
 		</GlobalContext.Provider>
 	)
